88f481e7cf5061c0a9c5cb74fdb709256b8642df
Shreya <ssen20170@gmail.com>
2023-03-19 15:15:17 +0530

    create server

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end(); // here return is important because otherwise it will execute the code after if block and throw error as res.write is not allowed after res.end
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.write("<html>");
+  res.write("<body><h3>Welcome to Node JS</h3></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
