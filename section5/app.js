05aa5be8e3be872d52d0689f1e49a52c6940526e
Shreya <ssen20170@gmail.com>
2023-03-20 16:18:02 +0530

    section 5

---
+++ section5/app.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const shopRouter = require("./routes/shop");
+const adminRouter = require("./routes/admin");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(8000);
